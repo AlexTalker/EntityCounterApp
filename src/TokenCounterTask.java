df869373feb631df28d4d6743ccfec0f9bb074d6
Alex Talker <alextalker@openmailbox.org>
2016-02-18 15:05:52 +0300

    Implement walking through dirs

--- src/TokenCounterTask.java
+++ src/TokenCounterTask.java
@@ -1,13 +1,18 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
 // TODO: Possibly merge with TokenCounter
 /**
  * Prepare Runnable object to handle console arguments with TokenCounter
@@ -15,6 +20,26 @@ import java.util.concurrent.Future;
  * @see TokenCounter
  */
 public class TokenCounterTask implements Runnable {
+
+    private class DirectoryVisitor extends SimpleFileVisitor<Path>{
+        private List<Future<?>> tasks = new LinkedList<>();
+        private ExecutorService executorService = Executors.newCachedThreadPool();
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+            throws IOException {
+            if(Files.probeContentType(file).equals("text/html")){
+                tasks.add(executorService.submit(new TokenCounterTask(file.toString())));
+            }
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException error){
+            // TODO: Notify or do not notify? That's a question!
+            return CONTINUE;
+        }
+    }
 
     private String path;
     private StringBuilder out = new StringBuilder();
@@ -29,7 +54,6 @@ public class TokenCounterTask implements Runnable {
     private void print(String s) {
         out.append(String.format("%s: %s\n", path, s));
     }
-
     @Override
     public void run() {
         long start = System.currentTimeMillis();
@@ -38,14 +62,32 @@ public class TokenCounterTask implements Runnable {
             try {
                 url = new URL(path);
             } catch (MalformedURLException e) {
-                url = new File(path).toURI().toURL();
+                File file = new File(path);
+                if(file.isDirectory()){
+                    DirectoryVisitor visitor = new DirectoryVisitor();
+                    Files.walkFileTree(file.toPath(), visitor);
+                    try {
+                        for (Future<?> task : visitor.tasks) {
+                            task.get();
+                        }
+                    }
+                    catch (InterruptedException error) {} // DIRTY
+                    catch (ExecutionException error) {}
+                    finally {
+                        visitor.executorService.shutdownNow();
+                    }
+                    return;
+                }
+                url = file.toURI().toURL();
             }
             try(InputStream in = url.openStream()) {
                 print(new TokenCounter(in).toString());
             }
+        } catch (SecurityException error) {
+            print("Fail because no access to read the file or directory.");
         } catch (MalformedURLException error) {
             print("Fail to understand the path.");
-        } catch (IOException e) {
+        } catch (IOException error) {
             print("Fail to read the source.");
         } finally {
             synchronized (System.out) {
